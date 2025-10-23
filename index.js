7c6446581e5646aa4bbff6ee9fc912f5be911425
Partha <168160706+Leoneix@users.noreply.github.com>
2025-10-23 18:57:33 +0530

    Password generator

--- index.js
+++ index.js
@@ -1,69 +1,123 @@
-let hasBlackJack = false;
-let isAlive = false;
-let message = "";
-let messageEl = document.getElementById("message-el");
-let sumEl = document.getElementById("sum-el");
-let cardEl = document.getElementById("card-el");
-let cards = [];
-let sum = 0;
+const char = [
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  " ",
+  "!",
+  '"',
+  "#",
+  "$",
+  "%",
+  "&",
+  "'",
+  "(",
+  ")",
+  "*",
+  "+",
+  ",",
+  "-",
+  ".",
+  "/",
+  ":",
+  ";",
+  "<",
+  "=",
+  ">",
+  "?",
+  "@",
+  "[",
+  "\\",
+  "]",
+  "^",
+  "_",
+  "`",
+  "{",
+  "|",
+  "}",
+  "~",
+];
 
-let player = {
-  name: "John",
-  chips: 145,
-};
+let pwdEl = document.getElementById("pw1");
+let pwdEl1 = document.getElementById("pw2");
 
-let playerEl = document.getElementById("player-el");
-playerEl.textContent = player.name + ": $" + player.chips;
-
-function startGame() {
-  isAlive = true;
-  if (isAlive === true && hasBlackJack === false) {
-    let firstCard = getRandomCard();
-    let secondCard = getRandomCard();
-    cards = [firstCard, secondCard];
-    sum = firstCard + secondCard;
-
-    renderGame();
-  }
-}
-
-function getRandomCard() {
-  let random = Math.floor(Math.random() * 13) + 1;
-  if (random > 10) {
-    random = 10;
-  } else if (random === 1) {
-    random = 11;
-  }
-
+function random(max) {
+  let random = Math.floor(Math.random() * max) + 1;
   return random;
 }
 
-function renderGame() {
-  if (sum <= 20) {
-    message = "Do you want to draw a new card?";
-  } else if (sum === 21) {
-    message = "Won!! Blackjack!!!";
-    hasBlackJack = true;
-  } else {
-    message = "Lost";
-    isAlive = false;
-  }
-  sumEl.textContent = "Sum:" + " " + sum;
+let pwd = [];
+let pwd1 = [];
+const length = 15;
 
-  for (let i = 0; i < cards.length; i++) {
-    cardEl.textContent += cards[i] + " ";
+function generate() {
+  let pwd = [];
+  let pwd1 = [];
+  for (let i = 0; i < length; i++) {
+    pwd.push(char[random(char.length)]);
   }
-  cardEl.textContent = "Cards:" + " " + cards[0] + " " + cards[1];
-  messageEl.textContent = message;
+  for (let i = 0; i < length; i++) {
+    pwd1.push(char[random(char.length)]);
+  }
+
+  pwdEl.textContent = pwd.join("");
+  pwdEl1.textContent = pwd1.join("");
 }
-
-function newCard() {
-  if (isAlive === true && hasBlackJack === false) {
-    let card = getRandomCard();
-    sum += card;
-    cards.push(card);
-    renderGame();
-  }
-}
-
-console.log(hasBlackJack);
